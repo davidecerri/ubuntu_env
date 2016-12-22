bcf764d67e81ac4affde6f541612c7d25f6ad550
Davide <davide@6sense.com>
2016-12-22 12:13:48 -0800

    adds isntall script and a readme template

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@ CURRENT=`pwd`
 
 if [ ! -d ~/.ansible_desktop_configuration_src ] ; then
    echo "Install Dependencies"
-   sudo apt-get install -y -qq python python-pip git openssh-server
+   sudo apt-get install -y -qq python python-dev python-pip git openssh-server libssl-dev
 
    echo "Install ansible"
    sudo pip install ansible
 
    echo "Get code from github"
-   git clone git@github.com:davidecerri/ubuntu_env.git ~/.ansible_desktop_configuration_src
+   git clone https://github.com/davidecerri/ubuntu_env.git ~/.ansible_desktop_configuration_src
    else
        echo " $HOME/.ansible_desktop_configuration_src is already cloned"
 fi
