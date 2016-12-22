be43f1802950f52f51d0097caa8ff19f1234ca3f
Davide <davide@6sense.com>
2016-12-22 11:48:06 -0800

    Set up a few things

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# ubuntu_env
-A config to have a working environment
+Ubuntu Setup
+=======================
+
+Requirements
+-----------
+
+    * curl,
+    * git,
+
+Run
+---
+
+```
+$ curl -L https://raw.githubusercontent.com/davidecerri/ubuntu_env/master/install.sh | sh
+```
+
+Result
+--------------------
+
+![Fail](https://media.giphy.com/media/PAujV4AqViWCA/giphy.gif)
+
+
---
+++ install.sh
@@ -0,0 +1,19 @@
+CURRENT=`pwd`
+
+if [ ! -d ~/.ansible_desktop_configuration_src ] ; then
+   echo "Install Dependencies"
+   sudo apt-get install -y -qq python python-dev python-pip git openssh-server libssl-dev
+
+   echo "Install ansible"
+   sudo pip install ansible
+
+   echo "Get code from github"
+   git clone https://github.com/davidecerri/ubuntu_env.git ~/.ansible_desktop_configuration_src
+   else
+       echo " $HOME/.ansible_desktop_configuration_src is already cloned"
+fi
+
+   cd  ~/.ansible_desktop_configuration_src
+   git pull
+
+   ansible-playbook -i hosts laptop.yml -c local -K
